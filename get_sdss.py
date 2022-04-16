4673cd297af456cdc5c57cca6d21955328d362fc
haifengchengguang <60420171+haifengchengguang@users.noreply.github.com>
2022-04-16 15:52:28 +0800

    Initial commit

---
+++ get_sdss.py
@@ -0,0 +1,31 @@
+import urllib.request  # 导入包
+import pandas as pd
+
+
+def getHtml(url):  # 获取html的内容
+    html = urllib.request.urlopen(url).read()  # bytes 如果不用read()，html会是一个↓
+    return html                                # http.client.HTTPResponse的变量
+
+
+def saveHtml(fileName, fileContent):
+    with open(fileName, "wb") as f:  # 以wb打开文件
+        f.write(fileContent)  # 写入
+
+
+def main():
+    Lmore15=pd.read_csv('200wLmore15_notrepeat.csv')
+    objID=Lmore15['objID']
+    for i in objID:
+        url = 'http://skyserver.sdss.org/dr17/VisualTools/explore/summary?id='+str(i)
+        html = getHtml(url)
+        saveHtml('sdss_objID/sdss_objID_'+str(i)+'.html', html)
+        #break
+    # url = "http://skyserver.sdss.org/dr17/VisualTools/explore/summary?id=1237668296598749280"  # url
+    # html = getHtml(url)  # 调用函数获取bytes
+    # saveHtml("sdss_1237668296598749280.html", html)  # 保存
+    print("保存网页完成")  # 提示语
+
+
+if __name__ == "__main__":  # 主函数
+    main()
+
